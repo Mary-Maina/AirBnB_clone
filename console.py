d1e935903f6f8cf42465808a34623d36740c4704
Mary-Maina <richellwanjiru@gmail.com>
2024-07-30 09:51:18 -0700

    updated console

--- console.py
+++ console.py
@@ -39,7 +39,7 @@ class HBNBCommand(cmd.Cmd):
         if len(commands) == 0:
             print("** class name missing **")
         elif commands[0] not in self.valid_classes:
-            print("** class doesn't exist**")
+            print("** class doesn't exist **")
         else:
             new_ins = BaseModel()
             new_ins.save()
